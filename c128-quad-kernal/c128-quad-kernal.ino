75c22a74b6574f76e21a2972fb0b8e23f207ea20
Barry Walker <barrywalker@gmail.com>
2021-01-01 18:24:50 -0500

    Add a "mode" which is toggled by holding RESTORE on power up

--- c128-quad-kernal/c128-quad-kernal.ino
+++ c128-quad-kernal/c128-quad-kernal.ino
@@ -5,7 +5,7 @@
    cycle through them. Can be paired with an additional adapter for the U32 C64 BASIC/Kernal to allow 4 separate
    kernals for the C64 portion of a 128 as well.
 
-   Copyright © 2020 Barry Walker
+   Copyright © 2021 Barry Walker
 
 */
 
@@ -18,7 +18,7 @@ const int KERNAL_COUNT      = 4;
 
 // On boot, we flash the selected kernal 5 times on the 128's LEDs
 const int FLASH_COUNT       = 5;
-const int LONG_FLASH_DELAY  = 500;
+const int LONG_FLASH_DELAY  = 250;
 const int SHORT_FLASH_DELAY = 100;
 const int RESET_DELAY       = 100;
 
@@ -42,18 +42,38 @@ const int RESET_PIN         = 8;  // PIN 11
 // We save the selected kernal in byte 0 of EEPROM
 int CURRENT_KERNAL_ID       = 0;
 
+// We save the selected mode in byte 1 of EEPROM
+int CURRENT_MODE_ID         = 1;
+
 int currentKernal = 0;
 
+// Keeps track of the state we're in
 enum State {
   IDLE,
   SELECT
 } state = IDLE;
+
+// Keeps track of the mode we're in. There are 2 modes:
+// BRIEF will only show the current kernal on the LEDs
+// when the kernal is changed, but keep both LEDs lit the
+// rest of the time.
+// DETAILED will show the current kernal on the LEDs at
+// all times - including when kernal 00 is selected which
+// will result in both LEDs being off.
+// To change modes, hold down the RESTORE key when you turn
+// the machine on. 
+enum Mode {
+  BRIEF,
+  DETAILED
+} mode = DETAILED;
 
 AceButton restore(RESTORE_PIN);
 
 void handleEvent(AceButton*, uint8_t, uint8_t);
 
 void setup() {
+  mode = loadMode();
+  
   // LEDs and address lines are OUTPUTs
   pinMode(LED1_PIN, OUTPUT);
   pinMode(LED2_PIN, OUTPUT);
@@ -62,8 +82,16 @@ void setup() {
 
   // Configure RESTORE and RESET for INPUTs with PULLUPs
   // Both lines are active low
-  pinMode(RESET_PIN, INPUT);
+  pinMode(RESET_PIN, INPUT_PULLUP);
   pinMode(RESTORE_PIN, INPUT_PULLUP);
+
+  // If RESTORE is held down at boot, change modes
+  if(digitalRead(RESTORE_PIN) == 0)
+  {
+    alternateLEDs(10,50);
+    changeMode();
+    saveMode(mode);
+  }
 
   restore.getButtonConfig()->setFeature(ButtonConfig::kFeatureLongPress);
   restore.getButtonConfig()->setFeature(ButtonConfig::kFeatureDoubleClick);
@@ -72,7 +100,6 @@ void setup() {
 
   currentKernal = loadKernal();
   doReset();
-  flashLEDs(currentKernal, FLASH_COUNT, LONG_FLASH_DELAY);
 }
 
 void loop() {
@@ -92,14 +119,12 @@ void handleEvent(AceButton* /*button*/, uint8_t eventType,
     case IDLE:
       if(longPressed) {
         state = SELECT;
-        blinkLEDs(BOTH_LEDS_ON, SHORT_FLASH_DELAY);
+        flickerLEDs();
         currentKernal = loadKernal();
         updateLEDs(currentKernal);
-
       }
       if(doubleClicked) {
         currentKernal = bumpKernal(currentKernal);
-        updateLEDs(currentKernal);
         doReset();
       }
       break;
@@ -107,7 +132,7 @@ void handleEvent(AceButton* /*button*/, uint8_t eventType,
     case SELECT:
       if(longPressed) {
         state = IDLE;
-        blinkLEDs(BOTH_LEDS_ON, SHORT_FLASH_DELAY);
+        flickerLEDs();
         doReset();
       }
       if(clicked) {
@@ -133,7 +158,7 @@ int bumpKernal(int value)
  */
 int loadKernal()
 {
-  return EEPROM.read(CURRENT_KERNAL_ID);
+  return loadFromEEPROM(CURRENT_KERNAL_ID);
 }
 
 /*
@@ -141,7 +166,39 @@ int loadKernal()
  */
 void saveKernal(int value)
 {
-  EEPROM.write(CURRENT_KERNAL_ID, value);
+  saveToEEPROM(CURRENT_KERNAL_ID, value);
+}
+
+/*
+ * Retrieve the current mode from EEPROM
+ */
+int loadMode()
+{
+  return loadFromEEPROM(CURRENT_MODE_ID);
+}
+
+/*
+ * Save the current mode to EEPROM
+ */
+void saveMode(int value)
+{
+  saveToEEPROM(CURRENT_MODE_ID, value);
+}
+
+/*
+ * Generic routine to load data from EEPROM
+ */
+int loadFromEEPROM(int location)
+{
+  return EEPROM.read(location);
+}
+
+/*
+ * Generic routine to save data to EEPROM
+ */
+void saveToEEPROM(int location, int value)
+{
+  EEPROM.write(location, value);
 }
 
 /*
@@ -186,6 +243,33 @@ void blinkLEDs(int value, int ms)
 }
 
 /*
+ * Indicate the current kernal on the LEDs by flashing the binary value
+ */
+void flashCurrentKernal()
+{
+  flashLEDs(currentKernal, FLASH_COUNT, LONG_FLASH_DELAY);
+}
+
+/*
+ * Do a quick flicker of the LEDs to show that selection has been accepted
+ */
+void flickerLEDs()
+{
+  blinkLEDs(BOTH_LEDS_ON, SHORT_FLASH_DELAY);
+}
+
+void alternateLEDs(int count, int ms)
+{
+  for(int i = 0; i < count; i++)
+  {
+    updateLEDs(1);
+    delay(ms);
+    updateLEDs(2);
+    delay(ms);
+  }
+}
+
+/*
  * Save the kernal to EEPROM, update the address lines and reset the machine
  */
 void doReset()
@@ -197,4 +281,16 @@ void doReset()
   delay(RESET_DELAY);
   digitalWrite(RESET_PIN, HIGH);
   pinMode(RESET_PIN, INPUT_PULLUP);
+  if(mode == BRIEF)
+  {
+    flashCurrentKernal();
+    updateLEDs(BOTH_LEDS_ON);
+  } else {
+    updateLEDs(currentKernal);
+  }
+}
+
+void changeMode()
+{
+  mode = mode == BRIEF ? DETAILED : BRIEF;
 }
